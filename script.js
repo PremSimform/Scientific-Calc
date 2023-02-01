67945c1a71a5bd7a823256c2d71793be88a14e57
PremSimform <prem.m@simformsolutions.com>
2023-02-01 12:06:32 +0530

    2nd Version changes

--- script.js
+++ script.js
@@ -1,77 +1,35 @@
-ip1 = document.getElementById("s1")
-ip2 = document.getElementById("s2")
+val1 = document.getElementById("n1")
+val2 = document.getElementById("n2")
 num1 = 0;
 op = "";
 num2 = "";
 arr = []
 
-ip = ip1
-ip2.value = 0;
-ip.focus();
+no = val1
+val2.value = 0;
+no.focus();
 
-ip.onblur = function () {
+no.onblur = function () {
     setTimeout(function () {
-        ip.focus();
-        val = ip1.value;
-        ip1.value = '';
-        ip1.value = val;
+        no.focus();
+        val = val1.value;
+        val1.value = '';
+        val1.value = val;
     }, 500);
 }
 
 function btnclick(x) {
-    y = ip1.value + x
-    ip1.value = y;
-
-    // num2 = num2 + x;
-    // ip2.value = eval(num2);
-    // calculate(num2);
+    y = val1.value + x
+    val1.value = y;
     calculate();
 
 }
 function opclick(x) {
-    // y = ip1.value + x
-    ip1.value += x;
-
-    // num2 = num2 + x;
-    // y = ip1.value + x
-    // ip1.value = y;
-    // arr = arr.concat(num2);
-    // arr = arr.concat(x);
-    // op = x;
-    // num1 = z;
-    // num2 = "";
-
-    // console.log(arr);
-
+    val1.value += x;
 }
-// function calculate(num) {
-
-//     switch (op) {
-//         case '+':
-//             z = Number(num1) + Number(num);
-//             break;
-//         case '-':
-//             z = Number(num1) - Number(num);
-//             break;
-//         case 'x':
-//             z = Number(num1) * Number(num);
-//             break;
-//         case '÷':
-//             z = Number(num1) / Number(num);
-//             break;
-//         default:
-//             z = Number(num2);
-//     }
-
-
-
-//     
-
-
-// }
 function btnclear() {
-    ip1.value = "";
-    ip2.value = "";
+    val1.value = "";
+    val2.value = "";
     num1 = 0;
     op = "";
     num2 = "";
@@ -79,76 +37,76 @@ function btnclear() {
 function btnsquare() {
 
 
-    ip1.value = ip2.value + "^2";
-    ip2.value = ip2.value ** 2;
+    val1.value = val2.value + "^2";
+    val2.value = val2.value ** 2;
 }
 function btnback() {
-    num2 = ip1.value.slice(0, -1)
-    ip1.value = num2;
+    num2 = val1.value.slice(0, -1)
+    val1.value = num2;
     if (num2 == "") {
-        ip2.value = 0;
+        val2.value = 0;
 
     }
     else {
-        ip2.value = eval(num2);
+        val2.value = eval(num2);
     }
 }
 
 function calculate() {
-    val = ip1.value;
+    val = val1.value;
 
     try {
         if (val == "") {
-            ip2.value = 0;
+            val2.value = 0;
 
         }
         else {
-            ip2.value = eval(val);
+            val2.value = eval(val);
         }
     }
     catch (err) {
-        ip2.value = "Error";
+        val2.value = "Error";
     }
 }
 function btnequals() {
-    ip1.value = ip2.value;
+    val1.value = val2.value;
 }
 function btnplusminus() {
-    ip2.value = ip2.value * -1;
+    val2.value = val2.value * -1;
 }
 function pi() {
-    ip1.value += 3.14159;
+    val1.value += 3.14159;
     calculate();
 }
 function e() {
-    ip1.value += 2.71828;
+    val1.value += 2.71828;
     calculate();
 }
 function btnfrac() {
-    ip1.value = ip2.value + "^-1"
-    ip2.value = ip2.value ** -1
+    val1.value = val2.value + "^-1"
+    val2.value = val2.value ** -1
 }
 function abs() {
-    ip2.value = Math.abs(ip2.value)
+    val2.value = Math.abs(val2.value)
 }
 function btnsqrt() {
 
-    ip1.value = '\u221A' + ip1.value
-    ip2.value = Math.sqrt(ip2.value)
+    val1.value = '\u221A' + val1.value
+    val2.value = Math.sqrt(val2.value)
 }
 function fact() {
     f = 1
-    n = ip1.value
+    n = val1.value
     for (i = 1; i <= n; i++) {
         f *= i;
 
     }
 
-    ip.value += '!'
-    ip2.value = f
+    no.value += '!'
+    val2.value = f
 }
 function exp() {
-    ip1.value += "**"
+    val1.value += "**"
 }
 
 
